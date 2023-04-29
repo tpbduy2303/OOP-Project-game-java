3d06742a77e695b384050f2b420059a5eadb7497
MDuc03 <101550783+MDuc03@users.noreply.github.com>
2023-04-29 21:37:23 +0700

    Add files via upload

--- OOP_Project_game/src/Main/GamePanel.java
+++ OOP_Project_game/src/Main/GamePanel.java
@@ -1,4 +1,45 @@
 package Main;
 
-public class GamePanel {
+import javax.swing.JPanel;
+
+import Inputs.KeyboardInputs;
+import Inputs.MouseInputs;
+
+import java.awt.Graphics;
+//import java.awt.event.KeyEvent;
+//import java.awt.event.KeyListener;
+public class GamePanel extends JPanel{
+    private MouseInputs mouseInputs;
+    private int xDel = 100, yDel = 100;
+    private int frames;
+    private long lastCheck = 0;
+    private int xDir = 1, yDir = 1;
+    public GamePanel(){
+        mouseInputs = new MouseInputs(this);
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+    public void changexDel(int value){
+        this.xDel += value; 
+        
+    }
+    public void changeyDel(int value){
+        this.yDel += value; 
+        
+    }
+    public void setRectPos(int x,int y){
+        this.xDel = x;
+        this.yDel = y;
+        
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        updateRectangel();
+        g.fillRect(xDel, yDel, 200, 50);
+    }
+    private void updateRectangel(){
+        xDel+=xDir;
+        yDel+=yDir;
+    }
 }
--- OOP_Project_game/src/Main/GameWindow.java
+++ OOP_Project_game/src/Main/GameWindow.java
@@ -1,4 +1,15 @@
 package Main;
 
+import javax.swing.JFrame;
+
 public class GameWindow {
+    private JFrame jframe;
+    public GameWindow(GamePanel gamePanel) {
+        jframe = new JFrame(null, null);
+        jframe.setSize(400, 400);
+        jframe.setVisible(true);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
