dfd2e8e434bf7b2f565221aeb9c34b7645a383db
Fakito <92991929+HungTouya@users.noreply.github.com>
2023-05-05 22:13:06 +0700

    Update Entity.java

--- OOP_Project_game/src/entities/Entity.java
+++ OOP_Project_game/src/entities/Entity.java
@@ -4,12 +4,32 @@ public abstract class Entity {
 
 	protected float x, y;
 	protected int width, height;
+	protected Rectangle hitbox;
+
 
 	public Entity(float x, float y, int width, int height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		initHitbox()
+	}
+
+}
+	protected void drawHitbox(Graphics g) {
+		g.drawRect((int) hitbox.x, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
+
+	}
+	protected void initHitbox() {
+		hitbox = new Rectangle((int)x,(int) y, width, height);
+	}
+	protected void updateHitbox() {
+		hitbox.x = (int) x;
+		hitbox.y = (int) y;
+	}
+
+	public Rectangle getHitbox() {
+		return hitbox;
 	}
 
 }
