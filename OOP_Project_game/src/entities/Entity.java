6fc0b3bb528a96b02d6ba6bf908b68237a4fcb92
tpbduy2303 <baoduy230303@gmail.com>
2023-05-08 22:31:55 +0700

    fix bug

--- OOP_Project_game/src/entities/Entity.java
+++ OOP_Project_game/src/entities/Entity.java
@@ -7,8 +7,6 @@ public abstract class Entity {
 
 	protected float x, y;
 	protected int width, height;
-	protected Rectangle hitbox;
-
 
 	protected Rectangle2D.Float hitbox;
 
@@ -17,25 +15,8 @@ public abstract class Entity {
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		initHitbox()
 	}
 
-}
-	protected void drawHitbox(Graphics g) {
-		g.drawRect((int) hitbox.x, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
-
-	}
-	protected void initHitbox() {
-		hitbox = new Rectangle((int)x,(int) y, width, height);
-	}
-	protected void updateHitbox() {
-		hitbox.x = (int) x;
-		hitbox.y = (int) y;
-	}
-
-	public Rectangle getHitbox() {
-		return hitbox;
-	}
 
 	protected void drawHitbox(Graphics g){
 		g.setColor(Color.BLUE);
@@ -45,12 +26,12 @@ public abstract class Entity {
 	protected void iniHitbox(float x, float y, float width, float height){
 		hitbox = new Rectangle2D.Float(x,y,width,height);
 	}
-//	public void updateHitbox(){
+	//	public void updateHitbox(){
 //		hitbox.x = (int)x;
 //		hitbox.y = (int)y;
 //	}
 	public Rectangle2D.Float getHitbox(){
-			return hitbox;
+		return hitbox;
 	}
 
 }
